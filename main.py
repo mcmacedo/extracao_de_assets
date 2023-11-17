f95c821cbe2c9f7a823b621777e748d9c14899aa
DanEl23 <danieleller1@hotmail.com>
2023-11-17 16:51:58 -0300

    Refatora o arquivo main para utilizar os novos modulos criados

--- main.py
+++ main.py
@@ -1,55 +1,53 @@
-def connect(string_de_conexao):
-    print(string_de_conexao)
-
-db_host="localhost"
-db_user="root"
-db_password="root123"
-db_name="extracao_de_assets"
-db_port=3306
+from database import create_connection, create_ativos_table
+from environ import get_argv_config, get_env_config
 
 
-def extrair_ativos():
-    # Extrai os ativos da tabela ativos
-    ativos = [
-        {
-            "id": 1,
-            "name": "Petrobras",
-            "gross_revenue": 1000000
-        },
-        {
-            "id": 2,
-            "name": "Vale",
-            "gross_revenue": 2000000
-        },
-        {
-            "id": 3,
-            "name": "Itau",
-            "gross_revenue": 3000000
-        },
-        {
-            "id": 4,
-            "name": "Banco do Brasil",
-            "gross_revenue": 4000000
-        },
-        {
-            "id": 5,
-            "name": "Bradesco",
-            "gross_revenue": 5000000
-        },
-    ]
-    return ativos
+def parse_ativos_to_dicts_list(conexao, busca_ativos):
+    ativos = busca_ativos(conexao)
+    ativos_tratados = []
+    
+    for ativo in ativos:
+        ativo_tratado = {
+            "id": ativo[0],
+            "asset_id": ativo[1],
+            "gross_revenue": ativo[2],
+            "label": ativo[3]
+        }
+        ativos_tratados.append(ativo_tratado)
+
+    return ativos_tratados
 
 
-def calcula_taxa_de_cambio(asset):
+def calcula_taxa_de_cambio(ativo):
     # Calcula a taxa de cambio
-    cambio = asset["gross_revenue"] = asset["gross_revenue"] * 5.5
+    cambio = ativo["gross_revenue"] = ativo["gross_revenue"] * 5.5
     return cambio
 
 
 if __name__ == "__main__":
-    conexao = connect(f"{db_host}, {db_user}, {db_password}, {db_name}, {db_port}")
-    ativos = extrair_ativos()
+    conexao = None
 
-    for asset in ativos:
-        cambio = calcula_taxa_de_cambio(asset)
-        print(cambio)
+    try:
+        environment = get_argv_config()
+        config = get_env_config(environment)
+        conexao = create_connection(config)
+        create_ativos_table(conexao)
+        ativos_tratados = parse_ativos_to_dicts_list(conexao, config['ativos_src'])
+        
+        for ativo in ativos_tratados:
+            cambio = calcula_taxa_de_cambio(ativo)
+            print(cambio)
+    except Exception as err:
+        print(f"Err no sistema - {err}")
+    finally:
+        if conexao:
+            conexao.close()
+            print("Conexão fechada com sucesso")
+
+
+# S.O.L.I.D.
+# S - Single Responsability Principle = Uma função ou classe deve ter apenas uma responsabilidade
+# O - Open Closed Principle = Uma função ou classe deve estar aberta para extensão, mas fechada para modificação
+# L - Liskov Substitution Principle (DEPOIS)
+# I - Interface Segregation Principle (DEPOIS)
+# D - Dependency Inversion Principle = Sua funções ou classes devem depender de interfaces, não de implementações
